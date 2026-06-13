d6d62ac255bc5ca3c1c6bb4fefe612f25cc834a2
dishanta-adhikari <dishantaadhikari.dev@gmail.com>
2026-06-13 16:56:40 +0530

    added-rate-limiter

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import noteRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "../middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -10,13 +11,12 @@ const PORT = process.env.PORT || 5001;
 
 // middleware
 app.use(express.json());
+app.use(rateLimiter);
 
 app.use("/api/notes", noteRoutes)
 
-connectDB();
-
-app.listen(PORT, () => {
-    console.log("Server is listening on POET:", PORT)
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is listening on POET:", PORT)
+    });
 });
-
-// mongodb+srv://dishanta:1234567890@cluster0.5hwcdny.mongodb.net/?appName=Cluster0
