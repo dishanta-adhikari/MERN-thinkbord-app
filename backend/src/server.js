495d3ed2b2f8a09dea36f17f5dbc414b451e37b7
dishanta-adhikari <dishantaadhikari.dev@gmail.com>
2026-06-13 21:21:42 +0530

    new-ratelimit-ui

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express, { json } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
 import noteRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "../middleware/rateLimiter.js";
 
 dotenv.config();
@@ -10,8 +12,12 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // middleware
+app.use(cors({
+    origin: "http://localhost:5173",
+}));
 app.use(express.json());
 app.use(rateLimiter);
+
 
 app.use("/api/notes", noteRoutes)
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar.jsx";
+import RateLimitedUI from "../components/RateLimitedUI.jsx";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes");
+        console.log(res.data);
+        setNotes(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        console.log("Status:", error.response?.status);
+        console.log("Data:", error.response?.data);
+
+        if (error.response?.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error("Failed to load notes");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNotes();
+  }, []);
+
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      {isRateLimited && <RateLimitedUI />}
+    </div>
+  );
 };
 
 export default HomePage;
