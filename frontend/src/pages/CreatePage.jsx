c98a3153f9f1835058f1d0121bcdad43489ce3eb
dishanta-adhikari <dishantaadhikari.dev@gmail.com>
2026-06-14 22:14:33 +0530

    axios-setup

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,14 +1,46 @@
 import React, { useState } from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { ArrowLeftIcon } from "lucide-react";
+import toast from "react-hot-toast";
+
+import api from "../lib/axios.js";
 
 const CreatePage = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (!title.trim() || !content.trim()) {
+      toast.error("All fields are required");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      await api.post("/notes", {
+        title,
+        content,
+      });
+      toast.success("Note created Succesfully!");
+      navigate("/");
+    } catch (error) {
+      console.log("Error creating note");
+      if (error.response.status === 429) {
+        toast.error("Slow down! You'r creating notes too fast", {
+          duration: 4000,
+          icon: "💀",
+        });
+      } else {
+        toast.error("Failed to create note");
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
